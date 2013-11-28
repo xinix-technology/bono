04c8cce029ab262d32b092f1f7899c07eb943bc1
Jafar Shadiq <jafar@xinix.co.id>
2013-11-28 12:33:10 +0700

    fix prettify url

--- src/Bono/Helper/URL.php
+++ src/Bono/Helper/URL.php
@@ -2,12 +2,16 @@
 
 namespace Bono\Helper;
 
+use \Bono\App;
+
 class URL {
     public static function base($uri = '') {
         $dir = $_SERVER['SCRIPT_NAME'];
+
         if (substr($dir, -4) === '.php') {
             $dir = dirname($dir);
         }
+
         if ($dir === '/') {
             $dir = '';
         }
@@ -16,12 +20,16 @@ class URL {
 
     public static function site($uri = '') {
         $dir = $_SERVER['SCRIPT_NAME'];
-        if (substr($dir, -4) === '.php') {
-            $dir = dirname($dir);
+
+        if (App::getInstance()->config('bono.prettifyURL')) {
+            if (substr($dir, -4) === '.php') {
+                $dir = dirname($dir);
+            }
+            if ($dir === '/') {
+                $dir = '';
+            }
         }
-        if ($dir === '/') {
-            $dir = '';
-        }
+
         return $dir.'/'.trim($uri, '/');
     }
 }
