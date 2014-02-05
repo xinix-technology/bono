9d4592096f5e0065d61be0d0adba54a6cedcb2aa
Ganesha <reekoheek@gmail.com>
2014-02-05 16:12:14 +0700

    introduce phpdoc for ControllerMiddleware

--- src/Bono/Middleware/ControllerMiddleware.php
+++ src/Bono/Middleware/ControllerMiddleware.php
@@ -1,8 +1,54 @@
 <?php
 
+/**
+ * Bono - PHP5 Web Framework
+ *
+ * MIT LICENSE
+ *
+ * Copyright (c) 2013 PT Sagara Xinix Solusitama
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author      Ganesha <reekoheek@gmail.com>
+ * @copyright   2013 PT Sagara Xinix Solusitama
+ * @link        http://xinix.co.id/products/bono
+ * @license     https://raw.github.com/xinix-technology/bono/master/LICENSE
+ * @package     Bono
+ *
+ */
 namespace Bono\Middleware;
 
+/**
+ * ControllerMiddleware
+ *
+ * Middleware to enable controller mechanism
+ *
+ * ControllerMiddleware needs Bono configuration:
+ *
+ * - bono.controllers
+ */
 class ControllerMiddleware extends \Slim\Middleware {
+
+    /**
+     * Instantiating controllers from configuration
+     */
     public function call() {
         $config = $this->app->config('bono.controllers');
         $mapping = $config['mapping'];
