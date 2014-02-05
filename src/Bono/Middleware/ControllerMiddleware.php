57e50f074efe64b01fd0962da043de5506a42fcc
Krisan Alfa Timur <krisan47@gmail.com>
2014-02-05 15:00:19 +0700

    Add Auth Middleware

--- src/Bono/Middleware/ControllerMiddleware.php
+++ src/Bono/Middleware/ControllerMiddleware.php
@@ -4,7 +4,7 @@ namespace Bono\Middleware;
 
 class ControllerMiddleware extends \Slim\Middleware {
     public function call() {
-        $config = $this->app->_config->get('bono.controllers');
+        $config = $this->app->config('norm.controllers');
         $mapping = $config['mapping'];
 
         $resourceUri = $this->app->request->getResourceUri();
