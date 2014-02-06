1b5e8ce6115b2cdaec7e3b6bb29d0c7ed8cf86b3
Ganesha <reekoheek@gmail.com>
2014-02-07 06:59:39 +0700

    introduce theme middleware

---
+++ src/Bono/Middleware/ThemeMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bono\Middleware;
+
+class ThemeMiddleware extends \Slim\Middleware {
+    public function call() {
+        $app = $this->app;
+
+        $config = $app->config('bono.theme');
+        $Theme = $config['class'];
+
+        if (isset($Theme)) {
+            $app->theme = new $Theme($app, $config);
+        }
+
+        $this->next->call();
+
+        $response = $app->response;
+        $template = $response->template();
+
+        if ($response->getStatus() == 200) {
+            $app->render($template, $response->data());
+        }
+    }
+}
