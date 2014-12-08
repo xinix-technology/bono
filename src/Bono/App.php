9441b3dcdd5984cd949b417cc91e26e07751b2d7
Farid Hidayat <e.faridhidayat@gmail.com>
2014-12-08 13:52:18 +0700

    added input content type sniff

--- src/Bono/App.php
+++ src/Bono/App.php
@@ -482,7 +482,13 @@ class App extends Slim
                 $Middleware = $k;
                 $options = $v;
             }
-            $m = new $Middleware();
+
+            // reekoheek: hack for middleware options
+            if ($options) {
+                $m = new $Middleware($options);
+            } else {
+                $m = new $Middleware();
+            }
             $m->options = $options;
             $this->add($m);
         }
@@ -628,6 +634,10 @@ class App extends Slim
         //     $this->add(new \Slim\Middleware\PrettyExceptions());
         // }
 
+        $this->add(new \Slim\Middleware\ContentTypes(array(
+            'multipart/form-data' => array($this, 'parseMultipartFormData'))
+        ));
+
         //Invoke middleware and application stack
         $this->middleware[0]->call();
 
@@ -670,5 +680,66 @@ class App extends Slim
         }
 
         throw new \ErrorException($errstr, $errno, 0, $errfile, $errline);
+    }
+
+    public function parseMultipartFormData($input)
+    {
+        $raw_data = $input;
+
+        if (empty($raw_data)) {
+            return;
+        }
+        // Fetch content and determine boundary
+        // $raw_data = file_get_contents('php://input');
+        $boundary = substr($raw_data, 0, strpos($raw_data, "\r\n"));
+
+        // Fetch each part
+        $parts = array_slice(explode($boundary, $raw_data), 1);
+        $data = array();
+
+        foreach ($parts as $part) {
+            // If this is the last part, break
+            if ($part == "--\r\n") break;
+
+            // Separate content from headers
+            $part = ltrim($part, "\r\n");
+            list($raw_headers, $body) = explode("\r\n\r\n", $part, 2);
+
+            // Parse the headers list
+            $raw_headers = explode("\r\n", $raw_headers);
+            $headers = array();
+            foreach ($raw_headers as $header) {
+                list($name, $value) = explode(':', $header);
+                $headers[strtolower($name)] = ltrim($value, ' ');
+            }
+
+            // Parse the Content-Disposition to get the field name, etc.
+            if (isset($headers['content-disposition'])) {
+                $filename = null;
+                preg_match(
+                    '/^(.+); *name="([^"]+)"(; *filename="([^"]+)")?/',
+                    $headers['content-disposition'],
+                    $matches
+                );
+                list(, $type, $name) = $matches;
+                isset($matches[4]) and $filename = $matches[4];
+
+                // handle your fields here
+                switch ($name) {
+                    // this is a file upload
+                    case 'userfile':
+                         file_put_contents($filename, $body);
+                         break;
+
+                    // default for all other files is to populate $data
+                    default:
+                         $data[$name] = substr($body, 0, strlen($body) - 2);
+                         break;
+                }
+            }
+
+        }
+
+        return $data;
     }
 }
