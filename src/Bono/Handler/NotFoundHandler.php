b2e1b7a17adcdbfdb92624734ccc2abc77e5dc32
Krisan Alfa Timur <krisan47@gmail.com>
2015-02-26 13:13:09 +0700

    [FIX] Renderring not found template.

--- src/Bono/Handler/NotFoundHandler.php
+++ src/Bono/Handler/NotFoundHandler.php
@@ -3,25 +3,37 @@
 namespace Bono\Handler;
 
 use Whoops\Run;
+use Bono\App;
+use Slim\View;
 
 class NotFoundHandler
 {
     public function handle()
     {
-        $app = \App::getInstance();
+        $app = App::getInstance();
 
-        if ($app->config('bono.debug') !== false) {
-            $view    = new \Slim\View();
-
-            $errorTemplate = '../templates/notFound.php';
-
-            if (is_readable($errorTemplate)) {
-                $view->setTemplatesDirectory('.');
-                return $view->display($errorTemplate, array(), 404);
+        if ($app->config('bono.debug') !== true) {
+            if (isset($app->response)) {
+                $app->response->setStatus(404);
             }
+
+            if (isset($app->theme)) {
+                $view = $app->theme->getView();
+                $errorTemplate = 'notFound';
+            } else {
+                $view = new View();
+                $errorTemplate = '../templates/notFound.php';
+
+                if (is_readable($errorTemplate)) {
+                    $view->setTemplatesDirectory('.');
+                }
+            }
+
+            return $view->display($errorTemplate, array(), 404);
         }
 
         $app->whoops->sendHttpCode(404);
+
         return call_user_func(array($app->whoops, Run::EXCEPTION_HANDLER), new \RuntimeException("404 Resource not found"));
     }
 }
