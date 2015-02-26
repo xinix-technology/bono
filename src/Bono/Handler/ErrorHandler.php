b541d7f6ef857cdb5d4117227d1e7bca11fb095e
Krisan Alfa Timur <krisan47@gmail.com>
2015-02-26 13:12:53 +0700

    [FIX] Renderring error template.

--- src/Bono/Handler/ErrorHandler.php
+++ src/Bono/Handler/ErrorHandler.php
@@ -4,29 +4,35 @@ namespace Bono\Handler;
 
 use Whoops\Run;
 use Bono\App;
+use Slim\View;
 
 class ErrorHandler
 {
     public function handle($e)
     {
-
         $app = App::getInstance();
 
         while (ob_get_level() > 0) ob_end_clean();
 
         // header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-        if ($app->config('bono.debug') !== false) {
-            if (isset($this->app->response)) {
-                $this->app->response->setStatus(500);
+        if ($app->config('bono.debug') !== true) {
+            if (isset($app->response)) {
+                $app->response->setStatus(500);
             }
 
-            $view = new \Slim\View();
-            $errorTemplate = '../templates/error.php';
+            if (isset($app->theme)) {
+                $view = $app->theme->getView();
+                $errorTemplate = 'error';
+            } else {
+                $view = new View();
+                $errorTemplate = '../templates/error.php';
 
-            if (is_readable($errorTemplate)) {
-                $view->setTemplatesDirectory('.');
-                return $view->display($errorTemplate, array(), 500);
+                if (is_readable($errorTemplate)) {
+                    $view->setTemplatesDirectory('.');
+                }
             }
+
+            return $view->display($errorTemplate, array('error' => $e), 500);
         }
 
         return call_user_func_array(array($app->whoops, Run::EXCEPTION_HANDLER), func_get_args());
