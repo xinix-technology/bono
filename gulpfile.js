cbe909e8d4112e3fe0153653876d5ad126042a1c
Ganesha <reekoheek@gmail.com>
2016-03-29 12:41:32 +0700

    MIGRATED from pas to gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const spawn = require('child_process').spawn;
+const fs = require('fs-promise');
+
+gulp.task('test', () => {
+  spawn('./vendor/bin/phpunit', ['test'], { stdio: 'inherit' });
+});
+
+gulp.task('coverage', () => {
+  fs.remove('./coverage')
+    .then(() => fs.mkdirp('./coverage'))
+    .then(() => {
+      spawn('./vendor/bin/phpunit', ['--whitelist=./src/', '--coverage-html=./coverage', 'test'], { stdio: 'inherit' });
+    });
+});
+
+gulp.task('default', () => {
+  console.log('Run:');
+  console.log('  test');
+  console.log('  coverage');
+});
